0af579457f28b2ef5621e3a5db5915dfa5c9f268
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-02-28 10:48:20 +0100

    Creando práctica 0

---
+++ P0/P0-Bal-Rui-Lui.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import load_iris
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Parte 1
+X,y = load_iris(True)
+X_n = np.array(X)
+last_2features = X_n[:,2:4])
