de7c482394d1ecdbd8769d6fadcce9776b6564dd
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-03-30 20:17:23 +0100

    Progresando en el ejercicio 1

--- P2/P2-BalRuiLui.py
+++ P2/P2-BalRuiLui.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-TRABAJO 2. 
-Nombre Estudiante: 
+TRABAJO 2.
+Nombre Estudiante: Luis Balderas Ruiz
+Aprendizaje Automático 2019
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # Fijamos la semilla
-np.random.seed(1)
+np.random.seed(77145416)
 
 
 def simula_unif(N, dim, rango):
 	return np.random.uniform(rango[0],rango[1],(N,dim))
 
 def simula_gaus(N, dim, sigma):
-    media = 0    
-    out = np.zeros((N,dim),np.float64)        
+    media = 0
+    out = np.zeros((N,dim),np.float64)
     for i in range(N):
-        # Para cada columna dim se emplea un sigma determinado. Es decir, para 
+        # Para cada columna dim se emplea un sigma determinado. Es decir, para
         # la primera columna se usará una N(0,sqrt(5)) y para la segunda N(0,sqrt(7))
         out[i,:] = np.random.normal(loc=media, scale=np.sqrt(sigma), size=dim)
-        
+
     return out
 
 
@@ -34,27 +35,75 @@ def simula_recta(intervalo):
     # y = a*x + b
     a = (y2-y1)/(x2-x1) # Calculo de la pendiente.
     b = y1 - a*x1       # Calculo del termino independiente.
-    
+
     return a, b
 
+def f_ej12(a,b,x,y):
+	return np.sign(y-a*x-b)
+
+"""
+Ejercicio 1: SOBRE LA COMPLEJIDAD DE H Y EL RUIDO
+"""
+
+class Ejercicio1:
+
+	def __init__(self):
+		print("####################################################")
+		print("####################################################")
+		print("EJERCICIO 1: SOBRE LA COMPLEJIDAD DE H Y EL RUIDO")
+		print("####################################################")
+		print("####################################################")
+
+	def Ej1(self):
+		print("Ejercicio 1. Dibujar una gráfica con la nube de puntos correspondiente")
+		print("a) N=50, dim=2, rango = [-50,50] con simula_unif")
+
+		listaN = simula_unif(50,2,[-50,50])
+		plt.scatter(listaN[:,0],listaN[:,1])
+		plt.title("Nube de puntos generada con simula_unif")
+		plt.show()
+
+		input("\n--- Pulsa una tecla para continuar ---\n")
+
+		print("b) N=50, dim=2, sigma=[5,7] con simula_gaus")
+
+		gauss = simula_gaus(50,2,[5,7])
+		plt.scatter(gauss[:,0],gauss[:,1])
+		plt.title("Nube de puntos generada con simula_gaus")
+		plt.show()
+
+		input("\n--- Pulsa una tecla para continuar ---\n")
+
+	def Ej2(self):
+		print("Ejercicio 2")
+
+		# En primer lugar, simulo los parámetros a, b para la recta y = ax+b en [-50,50]
+		print("Calculando la pendiente (a) y la ordenada en el origen (b) de la recta...")
+		a,b = simula_recta([-50,50])
+		print("Pendiente (a): ", a)
+		print("Término independiente (b): ", b)
+
+Ejerc1 = Ejercicio1()
+#Ejerc1.Ej1()
+Ejerc1.Ej2()
 
 
 ###############################################################################
 ###############################################################################
 ###############################################################################
 ###############################################################################
-print('EJERCICIO BONUS\n')
+#print('EJERCICIO BONUS\n')
 
 label4 = 1
 label8 = -1
 
 # Funcion para leer los datos
 def readData(file_x, file_y):
-	# Leemos los ficheros	
+	# Leemos los ficheros
 	datax = np.load(file_x)
 	datay = np.load(file_y)
 	y = []
-	x = []	
+	x = []
 	# Solo guardamos los datos cuya clase sea la 4 o la 8
 	for i in range(0,datay.size):
 		if datay[i] == 4 or datay[i] == 8:
@@ -63,8 +112,8 @@ def readData(file_x, file_y):
 			else:
 				y.append(label8)
 			x.append(np.array([1, datax[i][0], datax[i][1]]))
-			
+
 	x = np.array(x, np.float64)
 	y = np.array(y, np.float64)
-	
+
 	return x, y
