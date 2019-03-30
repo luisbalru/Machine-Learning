5f92c14a31d949a3c2d2658ffc47289c149b95f3
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-03-30 19:52:17 +0100

    Iniciando práctica 2

---
+++ P2/P2-BalRuiLui.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+TRABAJO 2. 
+Nombre Estudiante: 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Fijamos la semilla
+np.random.seed(1)
+
+
+def simula_unif(N, dim, rango):
+	return np.random.uniform(rango[0],rango[1],(N,dim))
+
+def simula_gaus(N, dim, sigma):
+    media = 0    
+    out = np.zeros((N,dim),np.float64)        
+    for i in range(N):
+        # Para cada columna dim se emplea un sigma determinado. Es decir, para 
+        # la primera columna se usará una N(0,sqrt(5)) y para la segunda N(0,sqrt(7))
+        out[i,:] = np.random.normal(loc=media, scale=np.sqrt(sigma), size=dim)
+        
+    return out
+
+
+def simula_recta(intervalo):
+    points = np.random.uniform(intervalo[0], intervalo[1], size=(2, 2))
+    x1 = points[0,0]
+    x2 = points[1,0]
+    y1 = points[0,1]
+    y2 = points[1,1]
+    # y = a*x + b
+    a = (y2-y1)/(x2-x1) # Calculo de la pendiente.
+    b = y1 - a*x1       # Calculo del termino independiente.
+    
+    return a, b
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+print('EJERCICIO BONUS\n')
+
+label4 = 1
+label8 = -1
+
+# Funcion para leer los datos
+def readData(file_x, file_y):
+	# Leemos los ficheros	
+	datax = np.load(file_x)
+	datay = np.load(file_y)
+	y = []
+	x = []	
+	# Solo guardamos los datos cuya clase sea la 4 o la 8
+	for i in range(0,datay.size):
+		if datay[i] == 4 or datay[i] == 8:
+			if datay[i] == 4:
+				y.append(label4)
+			else:
+				y.append(label8)
+			x.append(np.array([1, datax[i][0], datax[i][1]]))
+			
+	x = np.array(x, np.float64)
+	y = np.array(y, np.float64)
+	
+	return x, y
