c7585d4ebb326dd5a7b92896ccef54c095e34b84
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-05-14 09:02:33 +0200

    Empezando airfoil

---
+++ P3/airfoil.py
@@ -0,0 +1,31 @@
+# Práctica 3 Aprendiza Automático 2019
+# Autor: Luis Balderas Ruiz
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+print("AIRFOIL SELF-NOISE")
+
+names = ['Frequency','Angle-Attack','Chord-Length','Free-stream-velocity','Suction-thickness','SSPresure-level']
+data = pd.read_csv('./datos/airfoil_self_noise.dat',names = names,sep="\t") 
+
+
+print("Preprocesado")
+
+print("Matriz de correlación")
+corr_matrix = data.corr()
+k = 6 #number of variables for heatmap
+cols = corr_matrix.nlargest(k, 'Frequency')['Frequency'].index
+cm = np.corrcoef(data[cols].values.T)
+plt.subplots(figsize=(9,9))
+sns.set(font_scale=0.75)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+print("Pairplot")
+
+sns.set(font_scale=0.75)
+sns.pairplot(data[names],height=1.65)
+plt.show();
