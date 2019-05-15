2e962d3005795f217801a809602236fddd9dacc5
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-05-15 13:57:29 +0200

    Más preprocesado: outliers

--- P3/airfoil.py
+++ P3/airfoil.py
@@ -9,10 +9,11 @@ import numpy as np
 print("AIRFOIL SELF-NOISE")
 
 names = ['Frequency','Angle-Attack','Chord-Length','Free-stream-velocity','Suction-thickness','SSPresure-level']
-data = pd.read_csv('./datos/airfoil_self_noise.dat',names = names,sep="\t") 
+data = pd.read_csv('./datos/airfoil_self_noise.dat',names = names,sep="\t")
 
 
-print("Preprocesado")
+print("PREPROCESADO")
+
 
 print("Matriz de correlación")
 corr_matrix = data.corr()
@@ -24,8 +25,25 @@ sns.set(font_scale=0.75)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
 plt.show()
 
+input("\n--- Pulsa una tecla para continuar ---\n")
+
 print("Pairplot")
 
 sns.set(font_scale=0.75)
 sns.pairplot(data[names],height=1.65)
-plt.show();
+plt.show()
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+print("Resumen estadístico")
+
+tabla = data.describe()
+print(tabla)
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+plt.scatter(data['Frequency'],data['SSPresure-level'])
+plt.xlabel("Frequency")
+plt.title("Buscando outliers a través de Frequency")
+plt.ylabel("SSPresure-level")
+plt.show()
