0ed4eef3df939abc1ce869ad5daa6cfba6710adb
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-05-16 09:04:26 +0200

    Avanzando en el preprocesado

--- P3/airfoil.py
+++ P3/airfoil.py
@@ -5,6 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from scipy import stats
+from scipy.stats import skew
 
 print("AIRFOIL SELF-NOISE")
 
@@ -42,8 +44,86 @@ print(tabla)
 
 input("\n--- Pulsa una tecla para continuar ---\n")
 
+print("Outliers")
+
 plt.scatter(data['Frequency'],data['SSPresure-level'])
 plt.xlabel("Frequency")
 plt.title("Buscando outliers a través de Frequency")
 plt.ylabel("SSPresure-level")
 plt.show()
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+print("Eliminando posibles outliers")
+
+data = data.drop(data[data['SSPresure-level']<105].index)
+data = data.drop(data[data['SSPresure-level']>=140].index)
+
+plt.scatter(data['Frequency'],data['SSPresure-level'])
+plt.xlabel("Frequency")
+plt.title("Tras la eliminación de outliers")
+plt.ylabel("SSPresure-level")
+plt.show()
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+print("Distribuciones de las variables: Buscando asimetría y transformaciones")
+
+plt.subplots(figsize=(9,9))
+sns.distplot(data['Frequency']).set_title("Distribución de Frequency")
+plt.show()
+print("Asimetría de Frequency")
+print(skew(data['Frequency']))
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+plt.subplots(figsize=(9,9))
+sns.distplot(data['Angle-Attack']).set_title("Distribución de Angle-Attack")
+plt.show()
+print("Asimetría de Angle-Attack")
+print(skew(data['Angle-Attack']))
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+plt.subplots(figsize=(9,9))
+sns.distplot(data['Chord-Length']).set_title("Distribución de Chord-Length")
+plt.show()
+print("Asimetría de Chord-Length")
+print(skew(data['Chord-Length']))
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+plt.subplots(figsize=(9,9))
+sns.distplot(data['Free-stream-velocity']).set_title("Distribución de Free-stream-velocity")
+plt.show()
+print("Asimetría de Free-stream-velocity")
+print(skew(data['Free-stream-velocity']))
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+plt.subplots(figsize=(9,9))
+sns.distplot(data['Suction-thickness']).set_title("Distribución de Suction-thickness")
+plt.show()
+print("Asimetría de Suction-thickness")
+print(skew(data['Suction-thickness']))
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+print("Distribución de SSPresure-level")
+
+# Distribución con histograma
+plt.subplots(figsize=(9,9))
+sns.distplot(data['SSPresure-level']).set_title("Distribución de SSPresure-level")
+plt.show()
+# Gráfica de probabilidad
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+figura = plt.figure()
+res = stats.probplot(data['SSPresure-level'],plot=plt)
+plt.show()
+
+input("\n--- Pulsa una tecla para continuar ---\n")
+
+print("Skewness")
+print(skew(data['SSPresure-level']))
